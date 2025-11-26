73033edd921a571ae1ff8a4ea73f195b39840827
Artem Kushel <dgartemka@gmail.com>
2025-11-27 01:24:25 +0300

    another fix

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '@/views/HomeView.vue'
+import HomeView from '../views/HomeView.vue'
 import CatalogView from '../views/CatalogView.vue'
 import SupportView from '../views/SupportView.vue'
 import CartView from '../views/CartView.vue'
