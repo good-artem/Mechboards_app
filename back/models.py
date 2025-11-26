0a61c393484630157e2976f84348f909393bc6ec
Artem Kushel <dgartemka@gmail.com>
2025-11-26 20:59:28 +0300

    start

---
+++ back/models.py
@@ -0,0 +1,42 @@
+from sqlalchemy import ForeignKey, String, BigInteger
+from sqlalchemy.orm import Mapped, DeclarativeBase, mapped_column
+from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine
+
+
+engine = create_async_engine(url='sqlite+aiosqlite:///db.sqlite3', echo=True)
+
+async_session = async_sessionmaker(bind=engine, expire_on_commit=False)
+
+
+class Base(AsyncAttrs, DeclarativeBase):
+    pass
+
+
+class User(Base):
+    __tablename__ = 'users'
+    
+    id: Mapped[int] = mapped_column(primary_key=True)
+    tg_id = mapped_column(BigInteger)
+    name = mapped_column(String(128))
+    telephone_number = mapped_column(String(13))
+    address = mapped_column(String(128))    
+    count_orders = mapped_column(int)
+
+class Cart(Base):
+    __tablename__ = 'carts'
+    
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user: Mapped[int] = mapped_column(ForeignKey('users.id', ondelete='CASCADE'))
+
+class Task(Base):
+    __tablename__ = 'tasks'
+    
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(128))
+    completed: Mapped[bool] = mapped_column(default=False)
+    user: Mapped[int] = mapped_column(ForeignKey('users.id', ondelete='CASCADE'))
+
+
+async def init_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
